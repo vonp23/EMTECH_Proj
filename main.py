dfd570f7f7eeb4d84125262eec36ee01e48faa28
vonp23 <161335822+vonp23@users.noreply.github.com>
2024-05-18 12:54:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
 
-import cv2
 import numpy as np
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input as mobilenet_v2_preprocess_input
 
-model = tf.keras.models.load_model("mdl_wt.hdf5")
+# Disable TensorFlow INFO and WARNING logs
+tf.get_logger().setLevel('ERROR')
 
-uploaded_file = st.file_uploader("Choose a image file", type="jpg")
+# Load the model
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return tf.keras.models.load_model("mdl_wt.hdf5")
 
-map_dict = {0: 'dog',
-            1: 'horse',
-            2: 'elephant',
-            3: 'butterfly',
-            4: 'chicken',
-            5: 'cat',
-            6: 'cow',
-            7: 'sheep',
-            8: 'spider',
-            9: 'squirrel'}
+model = load_model()
 
-if uploaded_file is not None:
-    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    opencv_image = cv2.imdecode(file_bytes, 1)
-    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
-    resized = cv2.resize(opencv_image,(224,224))
+map_dict = {0: 'dog', 1: 'horse', 2: 'elephant', 3: 'butterfly', 4: 'chicken', 
+            5: 'cat', 6: 'cow', 7: 'sheep', 8: 'spider', 9: 'squirrel'}
 
-    st.image(opencv_image, channels="RGB")
-
+# Define function to make predictions
+def predict(image_data):
+    resized = cv2.resize(image_data, (224, 224))
     resized = mobilenet_v2_preprocess_input(resized)
-    img_reshape = resized[np.newaxis,...]
+    img_reshape = resized[np.newaxis, ...]
+    prediction = model.predict(img_reshape).argmax()
+    return map_dict[prediction]
 
-    Genrate_pred = st.button("Generate Prediction")
-    if Genrate_pred:
-        prediction = model.predict(img_reshape).argmax()
-        st.title("Predicted Label for the image is {}".format(map_dict[prediction]))
+# Main Streamlit app
+def main():
+    st.title("Image Classification with TensorFlow and Streamlit")
+
+    uploaded_file = st.file_uploader("Choose an image file", type="jpg")
+
+    if uploaded_file is not None:
+        image_data = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(image_data, 1)
+        opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+
+        st.image(opencv_image, caption='Uploaded Image', use_column_width=True)
+
+        if st.button('Classify'):
+            prediction = predict(opencv_image)
+            st.write(f"Prediction: {prediction}")
+
+if __name__ == "__main__":
+    main()
+
